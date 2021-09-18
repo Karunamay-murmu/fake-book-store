add97e7a537f020fff09cb5a0f1a9383a06241dd
Karunamay-murmu <Karunamaymurmu.km@gmail.com>
2021-09-18 12:45:47 +0530

    update core_setting

--- Ecommerce/settings/core_settings.py
+++ Ecommerce/settings/core_settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'staticfiles'),
+    BASE_DIR / 'static'
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
