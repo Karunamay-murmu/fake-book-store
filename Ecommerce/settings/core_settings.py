1ad21786a4f4b8b2f5525530f1e1acbb55a9796d
Karunamay-murmu <Karunamaymurmu.km@gmail.com>
2021-09-18 11:11:04 +0530

    update setting, update requirement.txt, add runtime.txt

--- Ecommerce/settings/core_settings.py
+++ Ecommerce/settings/core_settings.py
@@ -1,5 +1,6 @@
 import os
 import environ
+import django_heroku
 
 from pathlib import Path
 
@@ -78,10 +79,21 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd1r9orlpji0a0o',
+        'USER': 'qjogijgixjlrbu',
+        'PASSWORD': '8fdcfb7f151ecc71d2833952ab84c5772fa1a936619fd5b9a56e8ed96612a092',
+        'HOST': 'ec2-54-81-126-150.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
